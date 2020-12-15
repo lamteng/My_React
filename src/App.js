90019d36103f8904364aefb40ce7c00453edb5a0
Lam Teng <lamteng@hotmail.com>
2020-12-15 23:59:29 +0000

    applied state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+class App extends Component{ 
+  constructor(props) { 
+    super(props);
+    this.state={ // 宣告state物件，內包含一個變數percent
+      percent:"30%" 
+    }
+    this.changePercent=this.changePercent.bind(this); //綁定changePercent
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  changePercent(){ //加入changePercent函式
+    this.setState({percent:"70%"})
+  }
+
+  
+
+  render(){
+    return(
+      <div>
+        <div className="progress-back" style={{backgroundColor:"rgba(0,0,0,0.2)",width:"200px",height:"7px",borderRadius:"10px"}}>
+          <div className="progress-bar" style={{backgroundColor:"#fe5196",width:this.state.percent,height:"100%",borderRadius:"10px"}}></div>
+        </div>
+        <button onClick={this.changePercent}>轉換百分比 </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; //引入App
+import Layout from "./Layout";
 import reportWebVitals from './reportWebVitals';
 
-function Progress(){
-  const barWidth="50%";
-  return(
-      <div>
-      <div className="progress-back" style={{backgroundColor:"rgba(0,0,0,0.2)",width:"200px",height:"7px",borderRadius:"10px"}}>
-        <div className="progress-bar" style={{backgroundColor:"#fe5196",width:barWidth,height:"100%",borderRadius:"10px"}}></div>
-      </div>
-    </div>
-  );
-}
+
 ReactDOM.render(
   <div>
-      <Progress/>
+    <App/>
   </div>,
-  document.getElementById('root')
+document.getElementById('root')
 );
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
